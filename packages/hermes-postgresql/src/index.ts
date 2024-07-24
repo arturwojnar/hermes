1bae344f525c40a3e757fd5c921aa0454040d027
Artur Wojnar <contact@arturwojnar.dev>
2024-07-24 23:38:58 +0200

    feat: messages can be acknowledged based on the related LSN

--- packages/hermes-postgresql/src/index.ts
+++ packages/hermes-postgresql/src/index.ts
@@ -1,77 +1,211 @@
-import { Sql } from 'postgres'
+/* eslint-disable  @typescript-eslint/no-unused-vars */
+import { Duration, noop } from '@arturwojnar/hermes'
+import assert from 'assert'
+import postgres, { Options, PostgresType, Sql } from 'postgres'
+import { setTimeout } from 'timers/promises'
+import { AsyncOrSync } from 'ts-essentials'
+import { LogicalReplicationState, startLogicalReplication } from './logicalReplicationStream.js'
 
-// type Start = () => Promise<Stop>
-// type Stop = () => Promise<void>
-// type Publish<Event> = (event: Event | Event[]) => Promise<void>
-// type OutboxConsumer<Event> = {
-//   start: Start
-//   publish: Publish<Event>
-// }
-// type NowFunction = () => Date
-// type ErrorCallback = (error: unknown) => void
-// type ConsumerCreationParams<Event> = {
-//   // client: MongoClient
-//   // db: Db
-//   publish: (event: Event) => AsyncOrSync<void> | never
-//   /**
-//    * @defaultValue `default`
-//    */
-//   partitionKey?: string
-//   /**
-//    * @defaultValue 1000
-//    */
-//   waitAfterFailedPublishMs?: number
-//   /**
-//    * @defaultValue true
-//    */
-//   shouldDisposeOnSigterm?: boolean
-//   /**
-//    * Use with consciously and carefully.
-//    * When `true`, Hermes will be affecting many documents, resulting in much more I/O operations.
-//    * @defaultValue false
-//    */
-//   saveTimestamps?: boolean
-//   /**
-//    * @defaultValue `noop`
-//    */
-//   onFailedPublish?: ErrorCallback
-//   /**
-//    * @defaultValue `noop`
-//    */
-//   onDbError?: ErrorCallback
-//   /**
-//    * @defaultValue `() => new Date()`
-//    */
-//   now?: NowFunction
-// }
+type Start = () => Promise<Stop>
+type Stop = () => Promise<void>
+type Publish<Event> = (event: Event | Event[]) => Promise<void>
+type IOutboxConsumer<Event> = {
+  start: Start
+  publish: Publish<Event>
+  getDbConnection(): Sql
+}
+type NowFunction = () => Date
+type ErrorCallback = (error: unknown) => void
+type ConsumerCreationParams<Event> = {
+  getOptions: () => Options<Record<string, PostgresType>>
+  // db: Db
+  publish: (event: Event) => AsyncOrSync<void> | never
+  /**
+   * @defaultValue `default`
+   */
+  partitionKey?: string
+  /**
+   * @defaultValue 1000
+   */
+  waitAfterFailedPublishMs?: number
+  /**
+   * @defaultValue true
+   */
+  shouldDisposeOnSigterm?: boolean
+  /**
+   * Use with consciously and carefully.
+   * When `true`, Hermes will be affecting many documents, resulting in much more I/O operations.
+   * @defaultValue false
+   */
+  saveTimestamps?: boolean
+  /**
+   * @defaultValue `noop`
+   */
+  onFailedPublish?: ErrorCallback
+  /**
+   * @defaultValue `noop`
+   */
+  onDbError?: ErrorCallback
+  /**
+   * @defaultValue `() => new Date()`
+   */
+  now?: NowFunction
+}
+
+const PublicationName = `outbox_pub`
+const SlotName = `outbox_slot`
 
 export const migrate = async (sql: Sql) => {
   await sql`
-    DO $$
-    BEGIN
-      CREATE TABLE IF NOT EXISTS outbox (
-        position    BIGSERIAL     PRIMARY KEY,
-        event_type  VARCHAR(250)  NOT NULL,
-        data        JSONB         NOT NULL
-      );
+    CREATE TABLE IF NOT EXISTS outbox (
+      position    BIGSERIAL     PRIMARY KEY,
+      event_type  VARCHAR(250)  NOT NULL,
+      data        JSONB         NOT NULL
+    );
+  `
 
-      IF NOT EXISTS (SELECT 1 FROM pg_catalog.pg_publication WHERE pubname = 'outbox_pub') 
-      THEN
-          CREATE PUBLICATION outbox_pub FOR TABLE outbox;
-      END IF;
-    END $$;
-  `
-  await sql`
+  await sql.unsafe(`
     DO $$
     BEGIN
-      IF NOT EXISTS (SELECT 1 FROM pg_catalog.pg_publication WHERE pubname = 'outbox_pub') 
-      THEN
-          SELECT * FROM pg_create_logical_replication_slot('outbox_slot', 'pgoutput');
+      IF NOT EXISTS (SELECT 1 FROM pg_catalog.pg_publication WHERE pubname = '${PublicationName}') THEN
+        CREATE PUBLICATION ${PublicationName} FOR TABLE outbox;
       END IF;
     END $$;
-  `
+  `)
+
+  await sql.unsafe(`
+    DO $$
+    DECLARE
+      slot_created boolean;
+    BEGIN
+      IF NOT EXISTS (SELECT 1 FROM pg_catalog.pg_replication_slots WHERE slot_name = '${SlotName}')
+      THEN
+          SELECT * FROM INTO slot_created pg_create_logical_replication_slot('${SlotName}', 'pgoutput');
+      END IF;
+    END $$;
+  `)
 }
 
-// export const createOutboxConsumer = <Event>(params: ConsumerCreationParams<Event>): OutboxConsumer<Event> => {
-//   //
-// }
+export const createOutboxConsumer = <Event>(
+  params: ConsumerCreationParams<Event>,
+  // createClient: (options: Options<Record<string, PostgresType>>) => Sql,
+): OutboxConsumer<Event> => {
+  return new OutboxConsumer(params, (options: Options<Record<string, PostgresType>>) => postgres(options))
+}
+export class OutboxConsumer<Event> implements IOutboxConsumer<Event> {
+  private _sql: Sql | null = null
+
+  constructor(
+    private readonly _params: ConsumerCreationParams<Event>,
+    private readonly _createClient: (options: Options<Record<string, PostgresType>>) => Sql,
+  ) {}
+
+  getDbConnection() {
+    assert(this._sql, `A connection hasn't been yet established.`)
+    return this._sql
+  }
+
+  async start(): Promise<Stop> {
+    const { publish, getOptions } = this._params
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+    const sql = (this._sql = this._createClient({
+      ...getOptions(),
+    }))
+    const subscribeSql = this._createClient({
+      ...getOptions(),
+      publications: PublicationName,
+      transform: { column: {}, value: {}, row: {} },
+      max: 1,
+      fetch_types: false,
+      idle_timeout: undefined,
+      max_lifetime: null,
+      connection: {
+        application_name: 'hermes-postgresql',
+        replication: 'database',
+      },
+      // onclose: async function() {
+      //   if (ended)
+      //     return
+      //   stream = null
+      //   state.pid = state.secret = undefined
+      //   connected(await init(sql, slot, options.publications))
+      //   subscribers.forEach(event => event.forEach(({ onsubscribe }) => onsubscribe()))
+      // },
+    })
+
+    await migrate(sql)
+
+    // const { unsubscribe } = await sql.subscribe(
+    //   `insert:outbox`,
+    //   (row, a) => {
+    //     // { command, relation }
+    //     // Callback function for each row change
+    //     // tell about new event row over eg. websockets or do something else
+    //     console.info(row)
+    //     console.info(a)
+    //     // await publish(row as any)
+    //     // throw new Error()
+    //   },
+    //   () => {
+    //     // Callback on initial connect and potential reconnects
+    //     console.info('test')
+    //   },
+    // )
+    const replicationState: LogicalReplicationState = {
+      lastProcessedLsn: new Uint8Array(),
+      timestamp: new Date(),
+      publication: PublicationName,
+      slotName: SlotName,
+    }
+    startLogicalReplication(replicationState, subscribeSql, noop).catch((error) => {
+      console.error(error)
+    })
+
+    return async () => {
+      // unsubscribe()
+      await sql.end({ timeout: Duration.ofSeconds(5) })
+    }
+  }
+
+  async publish(event: Event | Event[]): Promise<void> {
+    await Promise.resolve(event)
+  }
+}
+
+const test = async () => {
+  const hermes = createOutboxConsumer({
+    getOptions() {
+      return {
+        host: 'localhost',
+        port: 5434,
+        database: 'hermes',
+        user: 'hermes',
+        password: 'hermes',
+      }
+    },
+    publish: (event) => {
+      return Promise.resolve()
+    },
+  })
+
+  const stop = await hermes.start()
+  const sql = hermes.getDbConnection()
+
+  process.on('SIGTERM', () => {
+    stop().catch(console.error)
+  })
+
+  let i = 0
+
+  while (++i) {
+    await setTimeout(Duration.ofSeconds(5))
+    const json = { name: 'AddTest' }
+    await sql`INSERT INTO outbox (event_type, data) VALUES('AddTest-${sql(i.toString())}', ${sql.json(json)})`
+  }
+}
+
+;(async () => {
+  await test()
+})().catch((error) => {
+  console.error(error)
+})
--- packages/hermes-postgresql/src/logicalReplicationStream.ts
+++ packages/hermes-postgresql/src/logicalReplicationStream.ts
@@ -3,16 +3,39 @@
 /* eslint-disable  @typescript-eslint/no-unsafe-call  */
 /* eslint-disable  @typescript-eslint/no-unsafe-member-access  */
 import { assertNever, noop } from '@arturwojnar/hermes'
+import { assert } from 'console'
 import { Sql } from 'postgres'
+import { AsyncOrSync } from 'ts-essentials'
 
 type Lsn = Uint8Array
 type LogicalReplicationState = {
-  lsn: Lsn
+  lastProcessedLsn: Lsn
   timestamp: Date
   publication: string
   slotName: string
 }
-type StateUpdate = { lsn: Lsn; timestamp: Date }
+type Transaction = { transactionId: number; lsn: Lsn; timestamp: Date }
+type OnDataProcessingResult =
+  | {
+      topLevelType: TopLevelType.PrimaryKeepaliveMessage
+      messageType: MessageType.Other
+      shouldPong: boolean
+    }
+  | {
+      topLevelType: TopLevelType.XLogData
+      messageType: MessageType.Begin
+      transaction: Transaction
+    }
+  | {
+      topLevelType: TopLevelType.XLogData
+      messageType: MessageType.Insert
+      transactionId: number
+    }
+  | {
+      topLevelType: TopLevelType.XLogData
+      messageType: MessageType.Other
+      letter: string
+    }
 
 enum Bytes {
   Int8 = 1,
@@ -60,11 +83,29 @@ const parseMessageType = (char: string): MessageType => {
 
   return MessageType.Other
 }
+const keepAliveResult = (shouldPong: boolean): OnDataProcessingResult => ({
+  topLevelType: TopLevelType.PrimaryKeepaliveMessage,
+  messageType: MessageType.Other,
+  shouldPong,
+})
+const offset = (offset = 0) => ({
+  add: (bytes: Bytes) => (offset += bytes),
+  addInt8: () => (offset += Bytes.Int8),
+  addInt16: () => (offset += Bytes.Int16),
+  addInt32: () => (offset += Bytes.Int32),
+  addInt64: () => (offset += Bytes.Int64),
+  value: () => offset,
+})
 
-const startLogicalReplication = async (state: LogicalReplicationState, sql: Sql) => {
-  state = { ...state }
+const startLogicalReplication = async (
+  state: LogicalReplicationState,
+  sql: Sql,
+  publish: (event: Event) => AsyncOrSync<void> | never,
+) => {
+  // state = { ...state }
 
-  const location = typeof state.lsn === 'undefined' ? '0/19EF078' : state.lsn.toString()
+  const transactions = new Map<number, Transaction>()
+  const location = typeof state.lastProcessedLsn === 'undefined' ? '0/19EF078' : state.lastProcessedLsn.toString()
   // const stream = await sql.unsafe(`
   //   START_REPLICATION SLOT ${state.slotName} LOGICAL ${location} (proto_version '1', publication_names '${state.publication}');
   // `)
@@ -80,88 +121,74 @@ const startLogicalReplication = async (state: LogicalReplicationState, sql: Sql)
     return sql.end()
   }
 
-  const updateState = (update?: StateUpdate) => {
-    if (update) {
-      state.lsn = update.lsn
-      state.timestamp = update.timestamp
+  const storeResult = (result: OnDataProcessingResult) => {
+    if (result.messageType === MessageType.Begin) {
+      transactions.set(result.transaction.transactionId, result.transaction)
+    } else if (result.messageType === MessageType.Insert) {
+      assert(transactions.has(result.transactionId))
+
+      const transaction = transactions.get(result.transactionId)
+      console.log(transaction)
+      // publish
     }
+
+    return result
   }
 
-  const pong = (lsn: Lsn) => {
-    // Creating Standby status update (F)
-    const reply = Buffer.alloc(Bytes.Int8 + Bytes.Int64 + Bytes.Int64 + Bytes.Int64 + Bytes.Int64 + Bytes.Int8)
-    reply[0] = 'r'.charCodeAt(0)
-    reply.fill(lsn, 1)
-    reply.writeBigInt64BE(toServerSystemClock(Date.now()), 25)
-    stream.write(reply)
+  const handleResult = (result: OnDataProcessingResult) => {
+    if (result.topLevelType === TopLevelType.PrimaryKeepaliveMessage && result.shouldPong) {
+      acknowledge()
+    }
   }
 
   // Standby status update (F)
   // https://www.postgresql.org/docs/10/protocol-replication.html
-  function acknowledge(lsn: Lsn) {
-    // const slice = lsn.split('/')
-    // const [upperWAL, lowerWAL] = [parseInt(slice[0], 16), parseInt(slice[1], 16)]
-    // const reply = Buffer.alloc(1 + 8 + 8 + 8 + 8 + 1)
-    // // Identifies the message as a receiver status update.
-    // reply.write('r', 0)
-    // // Int64. The location of the last WAL byte + 1 received and written to disk in the standby.
-    // reply.writeBigInt64BE(BigInt(lsn), 1)
-    // // Int64. The location of the last WAL byte + 1 flushed to disk in the standby.
-    // reply.writeBigInt64BE(BigInt(lsn), 9)
-    // // Int64. The location of the last WAL byte + 1 applied in the standby.
-    // reply.writeBigInt64BE(BigInt(lsn), 17)
-    // // Int64. The client's system clock at the time of transmission,
-    // // as microseconds since midnight on 2000-01-01.
-    // reply.writeBigInt64BE(toServerSystemClock(Date.now()), 25)
-    // // If 1, the client requests the server to reply to this message immediately.
-    // // This can be used to ping the server, to test if the connection is still healthy.
-    // reply.write('\x00', 33)
-    // stream.write(reply)
+  const acknowledge = () => {
+    const pos = offset()
+    const reply = Buffer.alloc(2 * Bytes.Int8 + 4 * Bytes.Int64)
+    // Identifies the message as Standby status update.
+    reply.write('r', pos.value())
+    // Int64. The location of the last WAL byte + 1 received and written to disk in the standby.
+    reply.fill(state.lastProcessedLsn, pos.addInt8())
+    // Int64. The location of the last WAL byte + 1 flushed to disk in the standby.
+    reply.fill(state.lastProcessedLsn, pos.addInt64())
+    // Int64. The location of the last WAL byte + 1 applied in the standby.
+    reply.fill(state.lastProcessedLsn, pos.addInt64())
+    // Int64. The client's system clock at the time of transmission.
+    reply.writeBigInt64BE(toServerSystemClock(Date.now()), pos.addInt64())
+    // If 1, the client requests the server to reply to this message immediately.
+    // This can be used to ping the server, to test if the connection is still healthy.
+    reply.write('\x00', pos.addInt64())
+    // Replying.
+    stream.write(reply)
   }
 
-  stream.on('data', (message: Buffer) => updateState(onData(message)))
+  stream.on('data', (message: Buffer) => handleResult(storeResult(onData(message))))
   stream.on('error', onError)
   stream.on('close', () => {
     close().catch(noop)
   })
-
-  // for await (const message of stream) {
-  //   if (message.tag !== 'insert') {
-  //     return
-  //   }
-
-  //   // Process your message here
-  //   console.log('Received message:', message)
-
-  //   // After processing, update the last acked LSN and acknowledge the message
-  //   state.lastProcessedId = message.lsn
-  //   await sendAcknowledgment(sql, location)
-
-  //   // Optionally, persist the lastAckedLSN to disk or database
-  //   // await persistLSN(lastAckedLSN)
-  // }
 }
 
-const onData = (message: Buffer): StateUpdate | undefined => {
+const onData = (message: Buffer): OnDataProcessingResult => {
   const topLevelType = parseTopLevelType(String.fromCharCode(message[0]))
 
   switch (topLevelType) {
     case TopLevelType.PrimaryKeepaliveMessage:
-      return handlePrimaryKeepAliveMessage(message)
+      return processPrimaryKeepAliveMessage(message)
     case TopLevelType.XLogData: {
       const data = message.subarray(XLogData_WalRecordStartByteNumber)
       const type = parseMessageType(String.fromCharCode(data[0]))
 
       switch (type) {
         case MessageType.Begin:
-          return handleBeginMessage(data)
+          return processBeginMessage(data)
         case MessageType.Insert:
-          return handleInsertMessage(data)
+          return processInsertMessage(data)
         default:
-          console.debug(`Skipped ${String.fromCharCode(data[0])}`)
+          return { topLevelType: TopLevelType.XLogData, messageType: MessageType.Other, letter: type }
       }
       // parse(x.subarray(25), state, sql.options.parsers, handle, options.transform)
-      return
     }
     default:
       assertNever(topLevelType)
@@ -176,7 +203,7 @@ const parse = (data: Buffer) => {
   // const char = (acc, [k, v]) => (acc[k.charCodeAt(0)] = v, acc)
 }
 
-// https://www.postgresql.org/docs/current/protocol-logicalrep-message-formats.html#PROTOCOL-LOGICALREP-MESSAGE-FORMATS-INSERT
+// https://www.postgresql.org/docs/current/protocol-logicalrep-message-fordatamats.html#PROTOCOL-LOGICALREP-MESSAGE-FORMATS-INSERT
 // https://www.postgresql.org/docs/current/protocol-logicalrep-message-formats.html#PROTOCOL-LOGICALREP-MESSAGE-FORMATS-TUPLEDATA
 /*
  * Byte1('I'). Identifies the message as an insert message.
@@ -192,33 +219,38 @@ Int16. Number of columns.
 
 Next, one of the following submessages appears for each column (except generated columns):
 
-Byte1('n'). Identifies the data as NULL value.
+Byte1('n'). Identifies the data as NULL value. 110.
 
 Or
 
-Byte1('u'). Identifies unchanged TOASTed value (the actual value is not sent).
+Byte1('u'). Identifies unchanged TOASTed value (the actual value is not sent). 117.
 
 Or
 
-Byte1('t'). Identifies the data as text formatted value.
+Byte1('t'). Identifies the data as text formatted value. 116.
 
 Or
 
-Byte1('b'). Identifies the data as binary formatted value.
+Byte1('b'). Identifies the data as binary formatted value. 98.
 
 Int32. Length of the column value.
 
 ByteN. The value of the column, either in binary or in text format. (As specified in the preceding format byte). N is the above length.
 */
-const handleInsertMessage = (data: Buffer) => {
+const processInsertMessage = (data: Buffer): OnDataProcessingResult => {
+  const transactionId = data.readInt32BE(Bytes.Int8)
   const TUPLE_START_BYTE = Bytes.Int8 + Bytes.Int32 + Bytes.Int32 + Bytes.Int8
   const tuplesBuffer = data.subarray(TUPLE_START_BYTE)
   // const columnsCount = tuplesBuffer.readUInt32BE(Bytes.Int16)
   // const columnsDescriptions = tuplesBuffer.readUInt32BE(Bytes.Int8 * 3)
 
   // assert(columnsCount === 3)
-  console.log(1)
-  return undefined
+
+  return {
+    topLevelType: TopLevelType.XLogData,
+    messageType: MessageType.Insert,
+    transactionId,
+  }
 }
 
 /*
@@ -227,11 +259,17 @@ const handleInsertMessage = (data: Buffer) => {
  * Int64 (TimestampTz). Commit timestamp of the transaction. The value is in number of microseconds since PostgreSQL epoch (2000-01-01).
  * Int32 (TransactionId). Xid of the transaction.
  */
-const handleBeginMessage = (data: Buffer) => {
-  const lsn = data.subarray(Bytes.Int8, Bytes.Int8 + Bytes.Int64)
-  const timestamp = toTimestamp(data.readBigInt64BE(Bytes.Int8 + Bytes.Int64))
+const processBeginMessage = (data: Buffer): OnDataProcessingResult => {
+  const pos = offset(Bytes.Int8)
+  const lsn = data.subarray(pos.value(), pos.addInt64())
+  const timestamp = toTimestamp(data.readBigInt64BE(pos.value()))
+  const transactionId = data.readInt32BE(pos.addInt64())
 
-  return { lsn, timestamp }
+  return {
+    topLevelType: TopLevelType.XLogData,
+    messageType: MessageType.Begin,
+    transaction: { transactionId, lsn, timestamp },
+  }
 }
 
 /*
@@ -240,15 +278,12 @@ const handleBeginMessage = (data: Buffer) => {
  * Int64. The server's system clock at the time of transmission, as microseconds since midnight on 2000-01-01.
  * Byte1. 1 means that the client should reply to this message as soon as possible, to avoid a timeout disconnect. 0 otherwise.
  */
-const handlePrimaryKeepAliveMessage = (data: Buffer) => {
+const processPrimaryKeepAliveMessage = (data: Buffer) => {
   if (!data[Bytes.Int8 + Bytes.Int64 + Bytes.Int64]) {
-    return
+    return keepAliveResult(false)
   }
 
-  const lsn = data.subarray(Bytes.Int8, Bytes.Int8 + Bytes.Int64)
-  const timestamp = toTimestamp(data.readBigInt64BE(Bytes.Int8 + Bytes.Int64))
-  // pong()
-  return { lsn, timestamp }
+  return keepAliveResult(true)
 }
 
 const toTimestamp = (value: bigint) => new Date(Date.UTC(2000, 0, 1) + Number(value / 1000n))
